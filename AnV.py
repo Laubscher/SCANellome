09b4a21fbfcacbe9b3d78c7dddce2a2c3336658a
Florian <florian.laubscher@unige.ch>
2022-11-22 17:49:30 +0100

    + ttktheme + refactore all layout in analyse function + initalisation & reset function

--- AnV.py
+++ AnV.py
@@ -2,6 +2,7 @@
 
 # Import modules - dependencies
 import os                                 # system
+import shutil
 import time
 from os import listdir
 import mappy as mp                        # mapper
@@ -10,44 +11,49 @@ import tkinter as tk                      # graphic interface
 from tkinter import ttk
 from tkinter import filedialog as fd
 from tkinter.messagebox import showinfo
-from tkinter import *
+#from tkinter import *
 import Dicodb                             # module that contain the database in a python dico
 
+from ttkthemes import ThemedTk
 
 # The software window
 
-new = tk.Tk()
-new.title('Anelloviruses')
-new.geometry("800x500")
+#main = tk.Tk()
+main = ThemedTk(theme="ubuntu", background=True)
 
-#Variables
+main.title('AnV')
+main.geometry("800x500")
 
-global workingRep
-global fastq1Path
-global fastq2Path
-global fastq1
-global fastq2
-global fq1
-global fq2
-global sampleList
-global sampleUniq
-global yAdd
-global pathLastDir
 global pb
-global minion
 
-minion = tk.IntVar(new, 0)   # 1 if Nanopore data 0 otherwise         # The check mark box
-minion.set(0)
+def start():
+  #Variables
+  global fastq1Path
+  global fastq2Path
+  global fastq1
+  global fastq2
+  global fq1
+  global fq2
+  global sampleList
+  global sampleUniq
+  global yAdd
+  global pathLastDir
+  global pairedLabel
+  global text1Label
+  global text2Label
+  global minion
+  global pathData
 
-pathData = os.path.expanduser("~/.AnV")                               # check if .AnV otherwise mk it
-if not os.path.exists(pathData):
-  os.mkdir(pathData)
 
-#####################################
-#               log                 #
-#####################################
+  pathData = os.path.expanduser("~/.AnV")                               # check if .AnV otherwise mk it
+  if not os.path.exists(pathData):
+    os.mkdir(pathData)
 
-if not os.path.exists(pathData + "/LOG"):
+  #####################################
+  #               log                 #
+  #####################################
+
+  if not os.path.exists(pathData + "/LOG"):
     os.mkdir(pathData + "/LOG")
     log = open(pathData + "/LOG/log.txt", "w")
     firstSessionTime = time.time()
@@ -55,62 +61,50 @@ if not os.path.exists(pathData + "/LOG"):
     log.close()
 
 
-log = open(pathData + "/LOG/log.txt", "a")
-sessionTime = time.time()
+  log = open(pathData + "/LOG/log.txt", "a")
+  sessionTime = time.time()
 
-log.write("\nSession start: " + str(sessionTime) +"\n")
+  log.write("\nSession start: " + str(sessionTime) +"\n")
 
-#####################################
+  #####################################
 
-#####################################
-#             USERDATA              #
-#####################################
+  #####################################
+  #             USERDATA              #
+  #####################################
 
-if not os.path.exists(pathData + "/USERDATA"):
-  os.mkdir(pathData + "/USERDATA")
-  log.write("USERDATA set up" + "\n")
+  if not os.path.exists(pathData + "/USERDATA"):
+    os.mkdir(pathData + "/USERDATA")
+    log.write("USERDATA set up" + "\n")
 
-pathLastDir = os.path.expanduser("~")
+  pathLastDir = os.path.expanduser("~")
 
-sampleUniq = set(listdir(pathData + "/USERDATA/"))     # list of all "samples" from past session - for each sample one directory in USERDATA/
+  sampleUniq = set(listdir(pathData + "/USERDATA/"))     # list of all "samples" from past session - for each sample one directory in USERDATA/
 
-log.write("List of samples found in USERDATA/: " + str(sampleUniq) + "\n")
+  log.write("List of samples found in USERDATA/: " + str(sampleUniq) + "\n")
 
-#####################################
-#               db                  #
-#####################################
-#make the fasta db from module Dicodb
-#####################################
+  #####################################
+  #               db                  #
+  #####################################
+  #make the fasta db from module Dicodb
+  #####################################
 
-if not os.path.exists(pathData + "/DATABASE"):
-  os.mkdir(pathData + "/DATABASE")
-  fasta=open(pathData + "/DATABASE/Anello.fasta", "w")
-  for entry in Dicodb.db:
-    if entry != "none":
-      fasta.write(">"+entry+"\n")
-      fasta.write(Dicodb.db[entry][5]+"\n")
-  fasta.close()
-  log.write("DATABASE set up" + "\n")
-#####################################
-log.close()
+  if not os.path.exists(pathData + "/DATABASE"):
+    os.mkdir(pathData + "/DATABASE")
+    fasta=open(pathData + "/DATABASE/Anello.fasta", "w")
+    for entry in Dicodb.db:
+      if entry != "none":
+        fasta.write(">"+entry+"\n")
+        fasta.write(Dicodb.db[entry][5]+"\n")
+    fasta.close()
+    log.write("DATABASE set up" + "\n")
+  #####################################
 
-yAdd = 150
-sampleList = []
+  log.close()
 
-fastq1Path = "<empty-mandatory>"
-fastq2Path = "<empty>"
+  yAdd = 200
+  sampleList = []
 
-fastq1 = StringVar()
-fastq2 = StringVar()
 
-fastq1.set(fastq1Path)
-fastq2.set(fastq2Path)
-
-fq1 = Label(new, textvariable=fastq1, bg="red", fg="black", relief=RIDGE)
-fq2 = Label(new, textvariable=fastq2, bg="yellow", fg="black", relief=RIDGE)
-pairedLabel = Label(new, text="(if paired)")
-text1Label = Label(new, text="Add a sample:")
-text2Label = Label(new, text="Sample list:")
 
 def select_file1():
     global fastq1Path
@@ -138,9 +132,9 @@ def select_file1():
     pathLastDir = "/".join(pathL)
 
     if fastq1Path.rstrip().split(".")[-1] == "fastq":
-        fq1.config(bg="green", fg="white")
+        fq1.config(background="darkorange1", foreground="white")
     else:
-        fq1.config(bg="yellow", fg="black")
+        fq1.config(background="yellow", foreground="black")
 
 def select_file2():
     global fastq2Path
@@ -149,7 +143,6 @@ def select_file2():
         ('Fastq files', '*.fastq'),
         ('All files', '*.*')
     )
-
     filename = fd.askopenfilename(
         title='Open a file',
         initialdir=pathLastDir,
@@ -168,9 +161,9 @@ def select_file2():
     pathLastDir = "/".join(pathL)
 
     if fastq2Path.rstrip().split(".")[-1] == "fastq":
-        fq2.config(bg="green", fg="white")
+        fq2.config(background="orange", foreground="white")
     else:
-        fq2.config(bg="yellow", fg="black")
+        fq2.config(background="yellow", foreground="black")
 
 def add_sample():
     global yAdd
@@ -191,29 +184,29 @@ def add_sample():
         os.mkdir(pathData + "/USERDATA/" + sampleName)
         sample = [sampleName, fastq1Path, fastq2Path, yAdd + 20, minion.get()]
         sampleList.append(sample)
-        labelListSample = Label(new, text=sampleName)
+        labelListSample = ttk.Label(main, text=sampleName)
         yAdd += 20
-        labelListSample.place(x=5, y=yAdd)
+        labelListSample.place(x=10, y=yAdd)
         sampleUniq.add(sampleName)
     print(minion.get())
     fastq1.set("<empty-mandatory>")
-    fq1.config(bg="red", fg="black")
+    fq1.config(background="gray", foreground="black")
     fastq2.set("<empty>")
-    fq2.config(bg="yellow", fg="black")
+    fq2.config(background="yellow", foreground="black")
 
 def run():
     global pb
+    global main
     log = open(pathData + "/LOG/log.txt", "a")
     log.write("Start mapping samples ... " + "\n")
 
-
     for s in sampleList:
         log.write("Mapping sample: " + str(s[0]) + "\n")
-        pb = ttk.Progressbar(new, orient='horizontal', mode='determinate', length=280)  # Progress bar
+        pb = ttk.Progressbar(main, orient='horizontal', mode='determinate', length=280)  # Progress bar
         pb.place(x=350, y=s[3])
-        new.update()
+        main.update()
         pb['value'] += 1
-        new.update()
+        main.update()
         try:
           curr = time.time()
           mapping(s[1], pathData + "/DATABASE/Anello.fasta", s[0])
@@ -221,15 +214,21 @@ def run():
         except:
           log.write("Error during mapping.. sample:" + str(s[0]) + "\n files may be corrupted!" + "\n")
           tk.messagebox.showinfo("Error..", "Error during mapping.. sample:" + str(s[0]) + "\n files may be corrupted!")
+
+          pb['value'] = 0
         else:
           pb['value'] += 1
-          new.update()
+          main.update()
           log.write("Mapping time sample: " + str(s[0]) + " " + str(time.time() - curr) + "\n")
 
-    save_button.place(x=200, y=120)
+    save_button = ttk.Button(
+        main,
+        text='Save',
+        command=file_save
+    )
+    save_button.place(x=290, y=155)
 
     log.close()
-
 
 def mapping(pathToFastq, db, nameS, type="single"):
     global pb
@@ -239,12 +238,12 @@ def mapping(pathToFastq, db, nameS, type="single"):
     preset = None
     if type == "Nanopore":
         preset = "map-ont"     # for Oxford Nanopore read mapping
-    elif type== "single":
-        preset ="sr"           # single-end short reads
+    elif type == "single":
+        preset = "sr"           # single-end short reads
     a = mp.Aligner(db, preset=preset)
 
     pb['value'] += 1
-    new.update()
+    main.update()
 
     for name, seq, qual in mp.fastx_read(pathToFastq):
         for hit in a.map(seq):
@@ -257,17 +256,11 @@ def mapping(pathToFastq, db, nameS, type="single"):
                 for k in range(0, hit.ctg_len):
                     genome_ref_covered[mgRef][0].append(0) # populate depth with 0
 
-
-                # info classification in a separated dict.
-                '''genome_ref_class[mgRef] = [hit.ctg.split(",")[1], hit.ctg.split(",")[2], hit.ctg.split(",")[3],
-                                           hit.ctg.split(",")[4], hit.ctg.split(",")[5]]
-                                                 #TODO keep only acc Number in fasta db make a correspendence table'''
-
             genome_ref_covered[mgRef][1] += 1
             for i in range(hit.r_st, hit.r_en):             # r_st  = ref start match, r_en -> end
                 genome_ref_covered[mgRef][0][i] += 1
     pb['value'] += 88
-    new.update()
+    main.update()
 
     for l in genome_ref_covered:
         genome_ref_covered[l].append(genome_ref_covered[l][2] - genome_ref_covered[l][0].count(0))  # 3 cov
@@ -277,7 +270,7 @@ def mapping(pathToFastq, db, nameS, type="single"):
         genome_ref_covered[l].append(median(genome_ref_covered[l][0]))  # 5 cov depth
 
     pb['value'] += 2
-    new.update()
+    main.update()
 
     # *# ############################# #*#
     # *#                               #*#
@@ -299,7 +292,7 @@ def mapping(pathToFastq, db, nameS, type="single"):
             dictSpecies[genome_ref_class[l][2]].add(l)
 
     pb['value'] += 2
-    new.update()
+    main.update()
 
     resultSp = set()
     for sp in listSpecies:  # for each sp keep the one with the best coverage
@@ -311,7 +304,7 @@ def mapping(pathToFastq, db, nameS, type="single"):
         resultSp.add(keep)
 
     pb['value'] += 2
-    new.update()
+    main.update()
 
     fichierCSV = open(pathData + "/USERDATA/" + nameS + "/species.csv", "w")
     #Sample Name, ACC. NUMBER, Reads, ref_len, cov, %cov, depth (median), GENUS, GROUP, SPECIES, GENOTYPE, HOST
@@ -321,12 +314,12 @@ def mapping(pathToFastq, db, nameS, type="single"):
             genome_ref_covered[accResultSp][5]) + ", " + genome_ref_class[accResultSp][0].split("=")[1]+ ", " + genome_ref_class[accResultSp][1].split("=")[1]+ ", " + genome_ref_class[accResultSp][2].split("=")[1] + ", " + genome_ref_class[accResultSp][3].split("=")[1] + ", " + genome_ref_class[accResultSp][4].split("=")[1] + "\n")
     fichierCSV.close()
     pb['value'] += 4
-    new.update()
+    main.update()
 
     return ("done")
 
 def file_save():
-    nameNewCsv = fd.asksaveasfile(mode='w',defaultextension=".csv")
+    nameNewCsv = fd.asksaveasfile(mode='w', defaultextension=".csv")
     text2save="Sample Name, ACC. NUMBER, Reads, ref_len, cov, %cov, depth (median), GENUS, GROUP, SPECIES, GENOTYPE, HOST\n"
     for s in sampleList:
         try:
@@ -339,55 +332,125 @@ def file_save():
     nameNewCsv.write(text2save)
     nameNewCsv.close
 
-# button
-open_button_fq1 = ttk.Button(
-    new,
-    text='Select Fastq File 1',
-    command=select_file1
-)
+def resetA():
+    global sampleUniq
+    sampleUniq = []
+    shutil.rmtree(pathData)
+    start()
+    for widgets in main.winfo_children():
+        widgets.destroy()
+    topButton()
 
-open_button_fq2 = ttk.Button(
-    new,
-    text='Select Fastq File 2',
-    command=select_file2
-)
 
-add_sample_button = ttk.Button(
-    new,
-    text='Add',
-    command=add_sample
-)
 
-run_button = ttk.Button(
-    new,
-    text='Run',
-    command=run
-)
+def analyse():
 
-save_button = ttk.Button(
-    new,
-    text='Save',
-    command=file_save
-)
+    global fastq1Path
+    global fastq2Path
+    global fastq1
+    global fastq2
+    global fq1
+    global fq2
+    global sampleList
+    global sampleUniq
+    global yAdd
+    global pathLastDir
+    global pairedLabel
+    global text1Label
+    global text2Label
+    global minion
+    global pathData
 
-#checkbox
 
-minion_check = ttk.Checkbutton(
-    new,
-    text="Oxford Nanopore",
-    variable=minion
+
+
+    global minion
+
+    minion = tk.IntVar(main, 0)  # 1 if Nanopore data 0 otherwise         # The check mark box
+    minion.set(0)
+
+    open_button_fq1 = ttk.Button(
+        main,
+        text='Select Fastq File 1',
+        command=select_file1
     )
 
-text1Label.place(x=5, y=15)
-open_button_fq1.place(x=5, y=40)
-fq1.place(x=230, y=45)
-open_button_fq2.place(x=5, y=70)
-fq2.place(x=230, y=75)
-pairedLabel.place(x=150, y=75)
-add_sample_button.place(x=5, y=120)
-run_button.place(x=100, y=120)
-text2Label.place(x=5, y=145)
-minion_check.place(x=230, y=15)
+    open_button_fq2 = ttk.Button(
+        main,
+        text='Select Fastq File 2',
+        command=select_file2
+    )
+
+    add_sample_button = ttk.Button(
+        main,
+        text='Add',
+        command=add_sample
+    )
+
+    run_button = ttk.Button(
+        main,
+        text='Run',
+        command=run
+    )
+
+
+
+    # checkbox
+
+    minion_check = ttk.Checkbutton(
+        main,
+        text="Oxford Nanopore",
+        variable=minion
+    )
+    fastq1Path = "<empty-mandatory>"
+    fastq2Path = "<empty>"
+
+    fastq1 = tk.StringVar()
+    fastq2 = tk.StringVar()
+
+    fastq1.set(fastq1Path)
+    fastq2.set(fastq2Path)
+
+    fq1 = ttk.Label(main, textvariable=fastq1, background="gray", foreground="black", relief="ridge")
+    fq2 = ttk.Label(main, textvariable=fastq2, background="yellow", foreground="black", relief="ridge")
+
+    pairedLabel = ttk.Label(main, text="(if paired)")
+
+    text1Label = ttk.Label(main, text="Add a sample:")
+    text2Label = ttk.Label(main, text="Sample list:")
+
+    text1Label.place(x=10, y=45)          # Add a sample
+    open_button_fq1.place(x=10, y=70)     #
+    open_button_fq2.place(x=10, y=105)    #
+    fq1.place(x=330, y=75)
+    fq2.place(x=330, y=110)               #
+    pairedLabel.place(x=220, y=110)       #
+    add_sample_button.place(x=10, y=155)  #
+    run_button.place(x=150, y=155)        #
+    text2Label.place(x=10, y=200)         #
+    minion_check.place(x=330, y=50)       # Oxford Nanopore
+
+
+# button
+
+def topButton():
+  reset_button = ttk.Button(
+    main,
+    text='reset',
+    command=resetA
+  )
+
+  analyse_button = ttk.Button(
+    main,
+    text='Analysis',
+    command=analyse
+  )
+
+  reset_button.place(x=670, y=-5)           #reset
+  analyse_button.place(x=-5, y=-5)          #analyze
 
 # run the application
-new.mainloop()
+
+topButton()
+start()
+main.mainloop()
