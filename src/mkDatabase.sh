f53773cf946431983c3977bde5f13962e96f2302
Florian <florian.laubscher@unige.ch>
2022-12-02 18:49:18 +0100

    path to database for Dicodb module as argument $1

--- src/mkDatabase.sh
+++ src/mkDatabase.sh
@@ -1,11 +1,11 @@
 
-#Anellovirus_2022.0.fasta #$1
+#$1 path to database #Anellovirus_2022.1.fasta #$1
 
 echo "global db" > Dicodb.py
 echo -n "db = { " >> Dicodb.py
-for headerInfo in $(grep ">" Anellovirus_2022.0.fasta | cut -d">" -f2):
+for headerInfo in $(grep ">" $1 | cut -d">" -f2):
  do echo -n "'"$(echo $headerInfo | cut -f1 -d",")"'"": [ '"$(echo $headerInfo | cut -f2 -d",")"','"$(echo $headerInfo | cut -f3 -d",")"','"$(echo $headerInfo | cut -f4 -d",")"','"$(echo $headerInfo | cut -f5 -d",")"','"$(echo $headerInfo | cut -f6 -d",") "','" >> Dicodb.py; done
- for seq in $(grep -A1 $(echo $headerInfo | cut -f1 -d",") Anellovirus_2022.0.fasta) ; do if [ ${seq::1} != ">" ] ; then echo -n $seq"']," >> Dicodb.py ; fi ;  done
+ for seq in $(grep -A1 $(echo $headerInfo | cut -f1 -d",") $1) ; do if [ ${seq::1} != ">" ] ; then echo -n $seq"']," >> Dicodb.py ; fi ;  done
 echo "'none': [] }" >> Dicodb.py 
 
 
