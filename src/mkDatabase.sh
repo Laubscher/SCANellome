c7ea4d48c030243db8eba2449f81587878a614f4
Florian <florian.laubscher@unige.ch>
2022-12-06 11:02:29 +0100

    rewrite mkDatabase.sh

--- src/mkDatabase.sh
+++ src/mkDatabase.sh
@@ -1,12 +1,12 @@
-
-#$1 path to database #Anellovirus_2022.1.fasta #$1
+#!/bin/bash
+#$1 path to database
 
 echo "global db" > Dicodb.py
 echo -n "db = { " >> Dicodb.py
-for headerInfo in $(grep ">" $1 | cut -d">" -f2):
- do echo -n "'"$(echo $headerInfo | cut -f1 -d",")"'"": [ '"$(echo $headerInfo | cut -f2 -d",")"','"$(echo $headerInfo | cut -f3 -d",")"','"$(echo $headerInfo | cut -f4 -d",")"','"$(echo $headerInfo | cut -f5 -d",")"','"$(echo $headerInfo | cut -f6 -d",") "','" >> Dicodb.py; done
- for seq in $(grep -A1 $(echo $headerInfo | cut -f1 -d",") $1) ; do if [ ${seq::1} != ">" ] ; then echo -n $seq"']," >> Dicodb.py ; fi ;  done
-echo "'none': [] }" >> Dicodb.py 
-
-
-
+for lane in $(cat $1):
+  do if [ ${lane::1} != ">" ] ;
+     then echo -n $lane"']," >> Dicodb.py;
+     else headerInfo=$(echo $lane | cut -d">" -f2);echo -n "'"$(echo $headerInfo | cut -f1 -d",")"'"": [ '"$(echo $headerInfo | cut -f2 -d",")"','"$(echo $headerInfo | cut -f3 -d",")"','"$(echo $headerInfo | cut -f4 -d",")"','"$(echo $headerInfo | cut -f5 -d",")"','"$(echo $headerInfo | cut -f6 -d",") "','" >> Dicodb.py;
+  fi;
+done;
+echo "'none': [] }" >> Dicodb.py
